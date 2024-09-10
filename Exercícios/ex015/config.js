1e53061ab66c7084b6d1a600898b990bcb460318
Victor Leonam <victorleonam16@gmail.com>
2024-09-10 20:46:38 -0300

    exericio finalizado

--- Exercícios/ex015/config.js
+++ Exercícios/ex015/config.js
@@ -7,45 +7,38 @@ function verificar(){
     let fem = document.getElementById('f')
     let idade = ano - anonasc
     let info = document.getElementById('info')
+    info.style.display = 'block'
+    let imagem = document.getElementById('imagem')
+    imagem.style.display = 'block'
     
     if(masc.checked && anonasc != "" ){
-        info.style.display = 'block'
-        let imagem = document.getElementById('imagem')
         if(idade < 13){
             info.innerHTML = `Você é uma criança do sexo <strong style="color: blue;"strong>masculino</strong> de ${idade} anos de idade`
             imagem.src = 'images/criança-homem.png'
-            imagem.style.display = 'block'
         }else if(idade < 18){
             info.innerHTML = `Você é um adolescente do sexo <strong style="color: blue;">masculino</strong> de ${idade} anos de idade`
-            imagem.style.display = 'block'
             imagem.src = 'images/adolescente-homem.png'
         }else if(idade < 50){
             info.innerHTML = `Você é um adulto do sexo <strong style="color: blue;">masculino</strong> de ${idade} anos de idade`
-             imagem.style.display = 'block'
+
             imagem.src = 'images/adulto-homem.png'
         }else if(idade >= 50){
             info.innerHTML = `Você é um idoso do sexo <strong style="color: blue;">masculino</strong> de ${idade} anos de idade`
-            imagem.style.display = 'block'
             imagem.src = 'images/idoso-homem.png'
         }
     }else if(fem.checked && anonasc != "" ){
-        info.style.display = 'block'
         let imagem = document.getElementById('imagem')
         if(idade < 13){
             info.innerHTML = `Você é uma criança do sexo <strong style="color: #F00096;">feminino</strong> de ${idade} anos de idade`
             imagem.src = 'images/criança-mulher.png'
-            imagem.style.display = 'block'
         }else if(idade < 18){
             info.innerHTML = `Você é um adolescente do sexo <strong style="color: #F00096;">feminino</strong> de ${idade} anos de idade`
-            imagem.style.display = 'block'
             imagem.src = 'images/adolescente-mulher.png'
         }else if(idade < 50){
             info.innerHTML = `Você é um adulto do sexo <strong style="color: #F00096;">feminino</strong> de ${idade} anos de idade`
-             imagem.style.display = 'block'
             imagem.src = 'images/adulto-mulher.png'
         }else if(idade >= 50){
             info.innerHTML = `Você é um idoso do sexo <strong style="color: #F00096;">feminino</strong> de ${idade} anos de idade`
-            imagem.style.display = 'block'
             imagem.src = 'images/idoso-mulher.png'
         }
     }else if(masc.checked == false && anonasc === ""){
